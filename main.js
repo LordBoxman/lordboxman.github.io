3ec254ea1db00657ad4977af82479bc8a73157c6
LordBoxman <33593001+LordBoxman@users.noreply.github.com>
2021-06-04 19:48:42 -0400

    updating files

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function blockClick(num){
   blocks = blocks + num;
   document.getElementById("blocks").innerHTML = blocks;
 };
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+    if(cookies >= cursorCost){
+        cursors = cursors + 1;
+    	blocks = blocks - cursorCost;
+        document.getElementById('cursors').innerHTML = cursors;
+        document.getElementById('cookies').innerHTML = cookies;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
+    document.getElementById('cursorCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+	
+	blockClick(cursors);
+	
+}, 1000);
