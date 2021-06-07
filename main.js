dd54077f751c95e19c94efa3c4eb1e971904510a
LordBoxman <33593001+LordBoxman@users.noreply.github.com>
2021-06-06 20:46:28 -0400

    big update

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 var blocks = 0;
-var cursors = 0;
 var blocksTotal = 0;
 var blocksPerSec = 0;
 var blocksPerClick = 1;
 
-function blockClick(num){
-  blocks = blocks + num;
-  blocksTotal = blocksTotal + num;
-  document.getElementById("blocks").innerHTML = blocks;
-  document.getElementById("blocksTotal").innerHTML = blocksTotal;
+var cursors = 0;
+
+
+function blockClick(){
+  blockAdd(blocksPerClick);
+};
+
+function blockAdd(num){
+	blocks = blocks + num;
+	blocksTotal = blocksTotal + num;
+	document.getElementById("blocks").innerHTML = blocks;
+	document.getElementById("statBlocks").innerHTML = blocks;
+	document.getElementById("statBlocksTotal").innerHTML = blocksTotal;
 };
 
 function buyCursor(){
@@ -17,8 +24,11 @@ function buyCursor(){
         cursors = cursors + 1;
 		blocksPerSec = blocksPerSec + 1;
 		blocks = blocks - cursorCost;
+		document.getElementById('blocks').innerHTML = blocks;
+		document.getElementById("statBlocks").innerHTML = blocks;
         document.getElementById('cursors').innerHTML = cursors;
-        document.getElementById('blocks').innerHTML = blocks;
+		document.getElementById('blocksPerSec').innerHTML = blocksPerSec;
+		document.getElementById('statBlocksPerSec').innerHTML = blocksPerSec;
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
     document.getElementById('cursorCost').innerHTML = nextCost;
@@ -26,6 +36,6 @@ function buyCursor(){
 
 window.setInterval(function(){
 	
-	blockClick(blocksPerSec);
+	blockAdd(blocksPerSec);
 	
 }, 1000);
